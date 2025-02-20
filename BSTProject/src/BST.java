c7fcf8da0a292f40a5a055b670a8424a7d8c500f
NadiaEs829 <nadia_amores@hotmail.com>
2025-02-20 18:38:31 +0000

    working end of 2/20

--- BSTProject/src/BST.java
+++ BSTProject/src/BST.java
@@ -3,4 +3,59 @@ import java.lang.Comparable;
 public class BST implements BSTInterface
 {
 
-}
+public BST(){
+//creates an empty BST
+root = null;
+}//end constructor 
+
+public void add(comparable newVal){
+    if(root = null)
+        root = new TreeNode(newVal);
+    else if(root.value >= newVal){
+        if(root.left = null)
+            root.left = new TreeNode(newVal);
+        else
+
+    }//end else if
+    
+}//end add method
+
+public void printInOrder(){
+    if(root != null){
+        printlnHelper(root.getLeft());
+        System.out.println(root.getValue);
+        printlnHelper(root.getRight());
+    }//end if
+}//end InOrder method
+
+public void printPreOrder(){
+    if(root != null){
+        System.out.println(root.getValue);
+        printlnHelper(root.getLeft());
+        printlnHelper(root.getRight());
+    }//end if 
+}//end method
+
+public void printPostOrder(){
+    if(root != null){
+        printlnHelper(root.getLeft());
+        printlnHelper(root.getRight());
+        System.out.println(root.getValue);
+    }//end if 
+}//end method
+
+
+private void addHelper(comparable newVal, root){
+    if( root.value >= newVal && root.left == null)
+        root.left = new TreeNode(newVal);
+    else if(root.value >= newVal)
+        addHelper(newVal, root.left);
+    else if(root.value < newVal && root.right == null)
+        root.right = new TreeNode(newVal);
+    else
+        addHelper(newVal, root.right);
+}//end addHelper method
+
+
+
+}//end class
--- BSTProject/src/BSTTester.java
+++ BSTProject/src/BSTTester.java
@@ -1,8 +1,8 @@
 /*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
- * TODO: Program Description
+ * TODO: Name: Nadia Escamilla
+ * TODO: Date: 2/20/25
+ * TODO: Class Period: 7
+ * TODO: Program Description: This program will create a BST and will be able to delete, add, and search for nodes based on the user input
  */
 
  import java.lang.Comparable;
@@ -11,4 +11,5 @@
    public static void main(String[] args) {
    
 	
- }
+ }//end main
+}//end class
